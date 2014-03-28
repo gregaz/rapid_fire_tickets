5b65fe1fa04422f3a05aa2a30eef2f4528131d86
Kevin Coleman <kevin.coleman@gatech.edu>
2014-03-28 12:23:03 -0400

    speed up add to cart process

--- home.js
+++ home.js
@@ -1,8 +1,13 @@
-if ($(".bpt_orangebutton").length != 0)
-{
-  $("#bpt_date_prices_1017125 .bpt_widget_price_table tr:nth-child(4) select").val(2);
-  $(".bpt_orangebutton").click(); 
-} else
-{
-  location.reload();
-}
+$( document ).ready(function() {
+
+  if ($(".bpt_orangebutton").length != 0)
+  {
+    $("#bpt_date_prices_1017125 .bpt_widget_price_table tr:nth-child(4) select").val(2);
+    $(".bpt_orangebutton").click(); 
+  } 
+  else
+  {
+    location.reload();
+  }
+
+});
